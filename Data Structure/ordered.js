4a499dd62d922c2fe23290301022f856312d98bc
bekiran <bekiranabbi@gamil.com>
2019-01-29 09:29:25 +0530

    Object Oriented Programs

--- Data Structure/ordered.js
+++ Data Structure/ordered.js
@@ -19,7 +19,7 @@ const read = require('readline-sync');
 /**
  * import the Utility class to use the functionalities of LinkedList.
  */
-var ll = require('../dataStructure/Implementation/linkedList');
+var ll = require('../Data Structure/Implementation/linkedList');
 /**
  * import the Utility class to use the functionaly of reading/writing a file.
  */
@@ -51,43 +51,3 @@ try {
 }
 
 
-/*
-var access = require('../dataStructure/Implementation/linkedList');
-var filestream = require('fs');
-var readline = require('readline-sync');
-var utility = require('../dataStructure/utility');
-
-function ordered() {
-
-    var string = filestream.readFileSync('number.txt', 'utf8');
-    var arr = string.split(' ');
-    console.log(arr);
-
-    var ord = new access.LinkedList;
-    for (let i = 0; i < arr.length; i++) {
-        ord.add(Number(arr[i]));
-    }
-
-    ord.addAscending();
-    ord.show();
-
-    var answer = readline.question("Enter the number do you want to search ");
-    if (!isNaN(answer)) {
-
-        if (ord.search(answer)) {
-            ord.remove(answer);
-        }
-        else {
-            ord.add(answer);
-            ord.addAscending();
-        }
-    }
-
-    ord.show();
-    var num = ord.getData();
-    utility.writeFile('number.txt', num);
-    console.log("The linked list elements are ");
-    ord.show();
-}
-
-ordered();*/
--- Object Oriented Programs/OOPs Programs/Inventory Management/inventoryManage.js
+++ Object Oriented Programs/OOPs Programs/Inventory Management/inventoryManage.js
@@ -1,21 +1,3 @@
-/*var utility = require("../../Utility/utility");
-var file = require("fs");
-flag = true;
-
-try {
-  var object = JSON.parse(filedata);
-
-  var filedata = file.readFileSync("inventoryManage.json", "utf8");
-} catch (err) {
-  console.log("error found!!!");
-  flag = false;
-}
-function manage() {
-    if(flag) utility.inventoryManage(object, file); //call inventoryMangage method
-
-}
-manage();
-*/
 var utility=require('../../Utility/utility');
 var ut
 var file=require('fs');
